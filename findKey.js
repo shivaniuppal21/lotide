469ddf97ce2b3c793584e0840e0f06bbef507fee
shivaniuppal21 <shivaniuppal21@gmail.com>
2021-01-06 20:57:33 +0000

    w1d3

--- findKey.js
+++ findKey.js
@@ -1,19 +1,19 @@
 const findKey = function(obj, callback) {
-    for (let key of Object.keys(obj)){
-        if (callback(obj[key])){
-            return key
-        }
+  for (let key of Object.keys(obj)) {
+    if (callback(obj[key])) {
+      return key;
     }
-}
+  }
+};
 
 
 console.log(findKey({
-    "Blue Hill": { stars: 1 },
-    "Akaleri":   { stars: 3 },
-    "noma":      { stars: 2 },
-    "elBulli":   { stars: 3 },
-    "Ora":       { stars: 2 },
-    "Akelarre":  { stars: 3 }
-  }, x => x.stars === 2)) // => "noma"
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
 
   
--- letterpositions.js
+++ letterpositions.js
@@ -1,46 +1,45 @@
 const eqArrays = function(actual, expected) {
-  if( actual.length === expected.length){
-      for( let i = 0; i < actual.length; i++){
-         if(actual[i]!== expected[i])
-         {
-             return false
-         }
-         if(i === actual.length -1){
-             return true
-         }
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
       }
-  }
-  return false
-  };
-  const assertArraysEquals = function (actual, expected){
-      if (eqArrays(actual,expected))
-      {
-          console.log("The arrays are identical!")
-      }else{
-          console.log("Not Equal!!")
+      if (i === actual.length - 1) {
+        return true;
       }
-      return assertArraysEquals
+    }
   }
+  return false;
+};
+const assertArraysEquals = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log("The arrays are identical!");
+  } else {
+    console.log("Not Equal!!");
+  }
+  return assertArraysEquals;
+};
 
 
-  assertArraysEquals([1, 2, 3], [1, 2, 3])
+assertArraysEquals([1, 2, 3], [1, 2, 3]);
 
 const letterPositions = function(allItems) {
-    const results = {}
-    for (let i=0;i<allItems.length;i++) {
-        if (results[allItems[i]]) {
-            results[allItems[i]].push(i);
+  const results = {};
+  for (let i = 0; i < allItems.length; i++) {
+    if (results[allItems[i]]) {
+      results[allItems[i]].push(i);
     } else if (allItems[i] !== " ") {
-            results[allItems[i]] = [];
-            results[allItems[i]].push(i)
-            } 
-      }
+      results[allItems[i]] = [];
+      results[allItems[i]].push(i);
+    }
+  }
     
-     return results;
+  return results;
 };
 
 
 
-console.log(letterPositions("lighthouse in the house"))
+console.log(letterPositions("lighthouse in the house"));
 assertArraysEquals(letterPositions("lighthouse in the house").l, [0]);
+assertArraysEquals(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
 
