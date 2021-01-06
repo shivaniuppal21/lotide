e381c0dbbf72ce3cde3f6460eca42af389ba01a6
shivaniuppal21 <shivaniuppal21@gmail.com>
2021-01-06 14:11:08 +0000

    w1d2

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,23 @@
 const eqArrays = function(actual, expected) {
-    if( actual.length === expected.length){
-        for( let i = 0; i < actual.length; i++){
-           if(actual[i]!== expected[i])
-           {
-               return false
-           }
-           if(i === actual.length -1){
-               return true
-           }
-        }
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      } if (i === actual.length - 1) {
+        return true;
+      }
     }
-    return false
-    };
-    const assertArraysEquals = function (actual, expected){
-        if (eqArrays(actual,expected))
-        {
-            console.log("The arrays are identical!")
-        }else{
-            console.log("Not Equal!!")
-        }
-        return assertArraysEquals
-    }
+  }
+  return false;
+};
+const assertArraysEquals = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log("The arrays are identical!");
+  } else {
+    console.log("Not Equal!!");
+  }
+  return assertArraysEquals;
+};
 
 
-    assertArraysEquals([1, 2, 3], [1, 2, 3])
+assertArraysEquals([1, 2, 3], [1, 2, 3]);
--- without.js
+++ without.js
@@ -1,38 +1,36 @@
 const eqArrays = function(actual, expected) {
-    if( actual.length === expected.length){
-        for( let i = 0; i < actual.length; i++){
-           if(actual[i]!== expected[i])
-           {
-               return false
-           }
-           if(i === actual.length -1){
-               return true
-           }
-        }
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+      if (i === actual.length - 1) {
+        return true;
+      }
     }
-    return false
-    };
-    const assertArraysEquals = function (actual, expected){
-        if (eqArrays(actual,expected))
-        {
-            console.log("The arrays are identical!")
-        }else{
-            console.log("Not Equal!!")
-        }
-        return assertArraysEquals
-    }
-    const without = function (source, itemsToRemove){
-    let newsource = source.filter(function(x){
-        return (itemsToRemove.indexOf(x) < 0)
-    })
-    return newsource
-    }
+  }
+  return false;
+};
+const assertArraysEquals = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log("The arrays are identical!");
+  } else {
+    console.log("Not Equal!!");
+  }
+  return assertArraysEquals;
+};
+const without = function(source, itemsToRemove) {
+  let newsource = source.filter(function(x) {
+    return (itemsToRemove.indexOf(x) < 0);
+  });
+  return newsource;
+};
 
-    console.log(without([1, 2, 3], [1,2]))
+console.log(without([1, 2, 3], [1,2]));
 
 const words = ["hello", "world", "lighthouse"];
 let newwords = without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
-assertArraysEquals(newwords,["hello", "world"])
+assertArraysEquals(newwords,["hello", "world"]);
 assertArraysEquals(words, ["hello", "world", "lighthouse"]);
 
